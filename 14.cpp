07f6a2dc7a8afebba1719e39f29449174cc18c50
Prakhar Trivedi <prakhartrivedi1980@gmail.com>
2026-08-08 00:28:29 +0530

    update Q14

--- 14.cpp
+++ 14.cpp
@@ -1,28 +1,13 @@
-<<<<<<< HEAD
 #include<bits/stdc++.h>
 using namespace std;
 string longprefixx(vector<string> &strs)
-{ 
+{
     if(strs.size()==1) return strs[0];
-    string ans = strs[0];
-    for(int i=1;i<strs.size()-1;i++)
-    {
-        string s1 = ans;
-        string s2 = strs[i];
-        
-=======
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-        if(strs.size()==1) return strs[0];
-        //vector<string> s = (strs.begin(),strs.end());
     string ans = strs[0];
     for(int i=1;i<strs.size();i++)
     {
         string s1 = ans;
         string s2 = strs[i];
-        //int size1 = s1.size();
-        //int size2 = s2.size();
 
         string temp = "";
         int final_size = s1.size()>s2.size() ? s2.size() : s1.size();
@@ -33,23 +18,18 @@ public:
             else break;                 
         }
         ans = temp;
-
         if(ans=="") return "";
     }
     return ans;
-<<<<<<< HEAD
-};
+}
 
 int main()
 {
-   vector<string> strs = {"reflower","flow","flight"};
+   vector<string> strs = {"flower","flow","flight"};
    /*cout<<strs[0]<<endl;  
    cout<<strs[1]<<endl;  
    cout<<strs[2]<<endl;*/  
    cout<<longprefixx(strs);
    
 }
-=======
-    }
-};
->>>>>>> 85d3f5df23382e40f05a2163ef9a4310eb028c8e
+
